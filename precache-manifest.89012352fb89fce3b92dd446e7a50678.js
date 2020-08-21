bfa44bf76e8c379a58b0be0c111d8fe5ed077403
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-08-21 14:10:15 -0500

    Updates

---
+++ precache-manifest.89012352fb89fce3b92dd446e7a50678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98eb9185c0e53057be1ce8ae03f1a2c",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "647537b7901a1bccb644",
+    "url": "/todo-app-react/static/css/main.6fb72117.chunk.css"
+  },
+  {
+    "revision": "4929853d8155e86b5686",
+    "url": "/todo-app-react/static/js/2.f60f5b64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.f60f5b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647537b7901a1bccb644",
+    "url": "/todo-app-react/static/js/main.34c54633.chunk.js"
+  },
+  {
+    "revision": "85cae369ffabab9cc3e3",
+    "url": "/todo-app-react/static/js/runtime-main.e0d73842.js"
+  }
+]);
